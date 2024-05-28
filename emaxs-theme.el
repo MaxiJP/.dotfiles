9c01f344b4b0cdccc3e3363f2b286738c91264ab
= <max.r.prime@gmail.com>
2024-05-28 23:34:52 +0100

    init commit

---
+++ emaxs-theme.el
@@ -0,0 +1,18 @@
+; +-----------------+
+; | [Maxmacs Theme] |
+; +-----------------+
+
+(deftheme emaxs "Created 2024-05-27.")
+
+; ------------------------
+;   Settings up Colours
+; ------------------------
+;
+
+; ----------------------
+;     Text rendering
+; ----------------------
+
+(set-face-attribute 'default nil :height 200)  ; Using Ubuntu Mono rn, with 200px size
+(set-face-attribute 'default nil :shadow '(5.0, "#77ee77", 0 . 0))
+
---
+++ init.el
@@ -0,0 +1,11 @@
+; misc for now
+(setq visible-bell t)           ; Just makes the warning more visible, for spotify's sake
+
+; ----------------------
+;     Text rendering
+; ----------------------
+
+(set-face-attribute 'default nil :height 200)  ; Using Ubuntu Mono rn, with 200px size
+
+(load-theme 'emaxs t)
+
